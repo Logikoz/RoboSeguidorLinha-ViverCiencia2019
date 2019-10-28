6376224d826e5b766c7523d7408b3e21250fecac
Logikoz <37851168+LogikozRC@users.noreply.github.com>
2019-10-27 19:22:54 -0500

    Adicionar arquivos de projeto.

---
+++ ReacaoRobo/ViewModels/TelaPrincipalViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReacaoRobo.ViewModels
+{
+    class TelaPrincipalViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        //campos
+
+        //propriedades
+
+        //metodos
+        public void ChangeValue(string property) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+    }
+}
---
+++ ReacaoRobo/Views/TelaPrincipalView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ReacaoRobo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ReacaoRobo.Views
+{
+    /// <summary>
+    /// Lógica interna para TelaPrincipalView.xaml
+    /// </summary>
+    public partial class TelaPrincipalView : Window
+    {
+        public TelaPrincipalView()
+        {
+            InitializeComponent();
+            DataContext = new TelaPrincipalViewModel();
+        }
+    }
+}
