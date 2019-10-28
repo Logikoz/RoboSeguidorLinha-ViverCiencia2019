425d12a22231521f689f161a56b93639b40d3426
Logikoz <37851168+LogikozRC@users.noreply.github.com>
2019-10-27 21:28:13 -0500

    Preparnado mvvm.

--- ReacaoRobo/ViewModels/TelaPrincipalViewModel.cs
+++ ReacaoRobo/ViewModels/TelaPrincipalViewModel.cs
@@ -4,6 +4,9 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ReacaoRobo.ViewModels
 {
@@ -11,10 +14,29 @@ namespace ReacaoRobo.ViewModels
     {
         public event PropertyChangedEventHandler PropertyChanged;
         //campos
-
+        private Border _statusRobo;
         //propriedades
-
+        public Border StatusRobo
+        {
+            get => _statusRobo;
+            set
+            {
+                _statusRobo = value;
+                ChangeValue("StatusRobo");
+            }
+        }
+        //construtor
+        public TelaPrincipalViewModel() => AlterarStatusRobo(true);
         //metodos
         public void ChangeValue(string property) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        private void AlterarStatusRobo(bool status)
+        {
+            Border border = new Border() { CornerRadius = new CornerRadius(10), Child = new TextBlock { Foreground = Brushes.White, Padding = new Thickness(4, 3, 4, 3) } };
+            if (status)
+                ((border.Child as TextBlock).Text, border.Background) = ("Ligado", Brushes.Green);
+            else
+                ((border.Child as TextBlock).Text, border.Background) = ("Desligado", Brushes.Red);
+            StatusRobo = border;
+        }
     }
 }
