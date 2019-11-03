94687abc8685da2156e26a71670403998fd0f2de
Logikoz <37851168+LogikozRC@users.noreply.github.com>
2019-11-03 18:36:12 -0500

    Documentando Model.

--- Software Desktop/ReacaoRobo/Models/TelaPrincipalModel.cs
+++ Software Desktop/ReacaoRobo/Models/TelaPrincipalModel.cs
@@ -1,12 +1,39 @@
 ﻿namespace ReacaoRobo.Models
 {
+    /// <summary>
+    /// Contem o modelo para as requisiçoes.
+    /// </summary>
     internal class ReacaoModel
     {
+        /// <summary>
+        /// Codigo de identificaçao do cartao.
+        /// </summary>
         public string CardID { get; set; }
+        /// <summary>
+        /// Caminho da imagem no computador.
+        /// </summary>
         public string Caminho { get; set; }
+        /// <summary>
+        /// Contem o tipo da reaçao <see cref="CategoriaReacoesEnum"/>.
+        /// </summary>
         public CategoriaReacoesEnum? Categoria { get; set; }
+        /// <summary>
+        /// Contem a descriçao da imagem.
+        /// </summary>
         public string Descricao { get; set; }
     }
-    public enum StatusRoboEnum { Conectado, Desconectado, Desconhecido }
-    public enum CategoriaReacoesEnum { Felicidade, Tristeza, Ansiedade, Raiva, Prazer }
+    /// <summary>
+    /// Possui uma lista de valores para definir o status atual do robo com base nas requisiçoes.
+    /// </summary>
+    public enum StatusRoboEnum
+    {
+        Conectado, Desconectado, Desconhecido
+    }
+    /// <summary>
+    /// Possui a lista de todas as reaçoes possíveis.
+    /// </summary>
+    public enum CategoriaReacoesEnum
+    {
+        Felicidade, Tristeza, Ansiedade, Raiva, Prazer
+    }
 }
