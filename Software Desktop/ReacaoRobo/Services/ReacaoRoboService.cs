61567f78e4873f61ee375c8359d402e4a1d18e69
Logikoz <37851168+LogikozRC@users.noreply.github.com>
2019-11-03 18:27:08 -0500

    Documentando Service.

--- Software Desktop/ReacaoRobo/Services/ReacaoRoboService.cs
+++ Software Desktop/ReacaoRobo/Services/ReacaoRoboService.cs
@@ -1,13 +1,24 @@
-﻿using RestSharp;
+﻿using ReacaoRobo.ViewModels;
+using RestSharp;
+using System;
 using System.Threading.Tasks;
 
 namespace ReacaoRobo.Services
 {
+    /// <summary>
+    /// Responsavel por fazer as requisiçoes das reaçoes.
+    /// </summary>
     internal class ReacaoRoboService
     {
-        public static async Task<IRestResponse> VerificarReacaoAsync(string uri, int timeOut = 1000)
+        /// <summary>
+        /// Faz a requisiçao e retorna a <see cref="IRestResponse"/> com o resultado da requisiçao
+        /// </summary>
+        /// <param name="uri">Uri do site que será feita a requisiçao, normalmente é uma propriedade em <see cref="TelaPrincipalViewModel"/></param>
+        /// <param name="timeOut">Tempo limite da requisiçao, por padrao é 1000ms ou uma proopriedade em <see cref="TelaPrincipalViewModel"/></param>
+        public static async Task<IRestResponse> VerificarReacaoAsync(Uri uri, int timeOut = 1000) => await new RestClient(uri)
         {
-            return await new RestClient(uri) { Timeout = timeOut }.ExecuteGetTaskAsync(new RestRequest(string.Empty, Method.GET, DataFormat.Json));
-        }
+            //setando o tempo limite da requisiçao.
+            Timeout = timeOut
+        }.ExecuteGetTaskAsync(new RestRequest(string.Empty, Method.GET, DataFormat.Json));
     }
 }
